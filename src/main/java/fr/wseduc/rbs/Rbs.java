72329860d4741fbcb24b643169e6e74f8fa7ef6c
cverdier <cyril.verdier@atos.net>
2014-07-30 17:26:33 +0200

    Types/Resources controllers layout and initialization

--- src/main/java/fr/wseduc/rbs/Rbs.java
+++ src/main/java/fr/wseduc/rbs/Rbs.java
@@ -1,12 +1,42 @@
 package fr.wseduc.rbs;
 
 import org.entcore.common.http.BaseServer;
+import org.entcore.common.http.filter.sql.ShareAndOwner;
+import org.entcore.common.service.impl.SqlCrudService;
+import org.entcore.common.share.impl.SqlShareService;
+import org.entcore.common.sql.SqlConf;
+import org.entcore.common.sql.SqlConfs;
+
+import fr.wseduc.rbs.controllers.ResourceController;
+import fr.wseduc.rbs.controllers.TypeController;
 
 public class Rbs extends BaseServer {
 	
 	@Override
 	public void start() {
 		super.start();
+		
+		SqlConf confType = SqlConfs.createConf(TypeController.class.getName());
+		confType.setTable("resource_type");
+		confType.setSchema(getSchema());
+		confType.setResourceIdLabel("typeId");
+		TypeController typeController = new TypeController();
+		typeController.setCrudService(new SqlCrudService(getSchema(), "resource_type"));
+		typeController.setShareService(new SqlShareService(getSchema(),"resource_type_shares",
+				getEventBus(vertx), securedActions, null));
+		addController(typeController);
+		
+		SqlConf confResource = SqlConfs.createConf(ResourceController.class.getName());
+		confResource.setTable("resource");
+		confResource.setSchema(getSchema());
+		confResource.setResourceIdLabel("resourceId");
+		ResourceController resourceController = new ResourceController();
+		resourceController.setCrudService(new SqlCrudService(getSchema(), "resource"));
+		resourceController.setShareService(new SqlShareService(getSchema(),"resource_shares",
+				getEventBus(vertx), securedActions, null));
+		addController(resourceController);
+		
+		setDefaultResourceFilter(new ShareAndOwner());
 	}
 
 }
---
+++ src/main/java/fr/wseduc/rbs/controllers/ResourceController.java
@@ -0,0 +1,7 @@
+package fr.wseduc.rbs.controllers;
+
+import org.entcore.common.controller.ControllerHelper;
+
+public class ResourceController extends ControllerHelper {
+
+}
