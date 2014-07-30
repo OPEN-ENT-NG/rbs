20689391683fa018ffd634460dcd8a6dfda3c761
dmtran <duy-minh.tran@atos.net>
2014-07-30 17:24:25 +0200

    SQL scripts to create schema and tables

---
+++ src/main/resources/sql/001-rbs.sql
@@ -0,0 +1,86 @@
+CREATE SCHEMA rbs;
+
+CREATE TABLE rbs.users (
+	id VARCHAR(36) NOT NULL PRIMARY KEY,
+	username VARCHAR(255)
+);
+
+CREATE TABLE rbs.groups (
+	id VARCHAR(36) NOT NULL PRIMARY KEY,
+	name VARCHAR(255)
+);
+
+CREATE TABLE rbs.members (
+	id VARCHAR(36) NOT NULL PRIMARY KEY,
+	user_id VARCHAR(36),
+	group_id VARCHAR(36),
+	CONSTRAINT user_fk FOREIGN KEY(user_id) REFERENCES rbs.users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT group_fk FOREIGN KEY(group_id) REFERENCES rbs.groups(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE rbs.resource_shares (
+	member_id VARCHAR(36) NOT NULL,
+	resource_id BIGINT NOT NULL,
+	action VARCHAR(255) NOT NULL,
+	CONSTRAINT share PRIMARY KEY (member_id, resource_id, action),
+	CONSTRAINT member_fk FOREIGN KEY(member_id) REFERENCES rbs.members(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE rbs.resource_type_shares (
+	member_id VARCHAR(36) NOT NULL,
+	resource_type_id BIGINT NOT NULL,
+	action VARCHAR(255) NOT NULL,
+	CONSTRAINT share PRIMARY KEY (member_id, resource_type_id, action),
+	CONSTRAINT member_fk FOREIGN KEY(member_id) REFERENCES rbs.members(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE rbs.scripts (
+	filename VARCHAR(255) NOT NULL PRIMARY KEY,
+	passed TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE FUNCTION rbs.merge_users(key VARCHAR, data VARCHAR) RETURNS VOID AS
+$$
+BEGIN
+    LOOP
+        UPDATE rbs.users SET username = data WHERE id = key;
+        IF found THEN
+            RETURN;
+        END IF;
+        BEGIN
+            INSERT INTO rbs.users(id,username) VALUES (key, data);
+            RETURN;
+        EXCEPTION WHEN unique_violation THEN
+        END;
+    END LOOP;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION rbs.insert_users_members() RETURNS TRIGGER AS $$
+    BEGIN
+		IF (TG_OP = 'INSERT') THEN
+            INSERT INTO rbs.members (id, user_id) VALUES (NEW.id, NEW.id);
+            RETURN NEW;
+        END IF;
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION rbs.insert_groups_members() RETURNS TRIGGER AS $$
+    BEGIN
+		IF (TG_OP = 'INSERT') THEN
+            INSERT INTO rbs.members (id, group_id) VALUES (NEW.id, NEW.id);
+            RETURN NEW;
+        END IF;
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER users_trigger
+AFTER INSERT ON rbs.users
+    FOR EACH ROW EXECUTE PROCEDURE rbs.insert_users_members();
+
+CREATE TRIGGER groups_trigger
+AFTER INSERT ON rbs.groups
+    FOR EACH ROW EXECUTE PROCEDURE rbs.insert_groups_members();
---
+++ src/main/resources/sql/002-rbs.sql
@@ -0,0 +1,58 @@
+CREATE TABLE rbs.resource_type(
+	id BIGSERIAL PRIMARY KEY,
+	owner VARCHAR(36) NOT NULL,
+	created TIMESTAMP NOT NULL DEFAULT NOW(),
+	modified TIMESTAMP NOT NULL DEFAULT NOW(),
+	visibility VARCHAR(9),
+	CONSTRAINT owner_fk FOREIGN KEY(owner) REFERENCES rbs.users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+
+	name VARCHAR(255) NOT NULL,
+	validation BOOLEAN NOT NULL
+);
+
+ALTER TABLE rbs.resource_type_shares ADD CONSTRAINT resource_type_fk FOREIGN KEY(resource_type_id) REFERENCES rbs.resource_type(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+CREATE TABLE rbs.resource(
+	id BIGSERIAL PRIMARY KEY,
+	owner VARCHAR(36) NOT NULL,
+	created TIMESTAMP NOT NULL DEFAULT NOW(),
+	modified TIMESTAMP NOT NULL DEFAULT NOW(),
+	visibility VARCHAR(9),
+	CONSTRAINT owner_fk FOREIGN KEY(owner) REFERENCES rbs.users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+
+	name VARCHAR(255) NOT NULL,
+	description TEXT,
+	icon VARCHAR(255),
+	periodic_booking BOOLEAN NOT NULL,
+	is_available BOOLEAN NOT NULL,
+	min_delay BIGINT,
+	max_delay BIGINT,
+	type_id BIGINT NOT NULL,
+	school_id VARCHAR(255) NOT NULL,
+	CONSTRAINT type_fk FOREIGN KEY(type_id) REFERENCES rbs.resource_type(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT valid_delays CHECK (min_delay >= 0 AND max_delay >= min_delay)
+);
+
+ALTER TABLE rbs.resource_shares ADD CONSTRAINT resource_fk FOREIGN KEY(resource_id) REFERENCES rbs.resource(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+CREATE TABLE rbs.booking(
+	id BIGSERIAL PRIMARY KEY,
+	resource_id BIGSERIAL NOT NULL,
+	booker_id VARCHAR(36) NOT NULL,
+	booking_reason TEXT,
+	created TIMESTAMP NOT NULL DEFAULT NOW(),
+	modified TIMESTAMP NOT NULL DEFAULT NOW(),
+	start_date TIMESTAMP NOT NULL,
+	end_date TIMESTAMP NOT NULL,
+	status SMALLINT,
+	moderator_id VARCHAR(36),
+	refusal_reason TEXT,
+	parent_booking_id BIGSERIAL,
+	CONSTRAINT resource_fk FOREIGN KEY(resource_id) REFERENCES rbs.resource(id) ON UPDATE CASCADE,
+	CONSTRAINT booker_fk FOREIGN KEY(booker_id) REFERENCES rbs.users(id) ON UPDATE CASCADE,
+	CONSTRAINT moderator_fk FOREIGN KEY(booker_id) REFERENCES rbs.users(id) ON UPDATE CASCADE,
+	CONSTRAINT parent_booking_fk FOREIGN KEY(parent_booking_id) REFERENCES rbs.booking(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
