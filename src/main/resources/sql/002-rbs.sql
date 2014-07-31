4c9422b50eb09a75c632132fd9ec044a0dbc8e30
dmtran <duy-minh.tran@atos.net>
2014-07-31 12:03:22 +0200

    SQL : move attribute school_id from table resource to table resource_type

--- src/main/resources/sql/002-rbs.sql
+++ src/main/resources/sql/002-rbs.sql
@@ -7,7 +7,8 @@ CREATE TABLE rbs.resource_type(
 	CONSTRAINT type_owner_fk FOREIGN KEY(owner) REFERENCES rbs.users(id) ON UPDATE CASCADE ON DELETE CASCADE,
 
 	name VARCHAR(255) NOT NULL,
-	validation BOOLEAN NOT NULL
+	validation BOOLEAN NOT NULL,
+	school_id VARCHAR(255) NOT NULL
 );
 
 ALTER TABLE rbs.resource_type_shares ADD CONSTRAINT resource_type_fk FOREIGN KEY(resource_type_id) REFERENCES rbs.resource_type(id) ON UPDATE CASCADE ON DELETE CASCADE;
@@ -29,7 +30,6 @@ CREATE TABLE rbs.resource(
 	min_delay BIGINT,
 	max_delay BIGINT,
 	type_id BIGINT NOT NULL,
-	school_id VARCHAR(255) NOT NULL,
 	CONSTRAINT type_fk FOREIGN KEY(type_id) REFERENCES rbs.resource_type(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT valid_delays CHECK (min_delay >= 0 AND max_delay >= min_delay)
 );
