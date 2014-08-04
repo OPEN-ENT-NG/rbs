fd1aa99fc48c1e14a387e6dc88df257c88147396
cverdier <cyril.verdier@atos.net>
2014-08-04 14:42:35 +0200

    Model load resourceTypes and resources fix

--- src/main/resources/public/js/model.js
+++ src/main/resources/public/js/model.js
@@ -49,23 +49,25 @@ model.build = function(){
 
 	this.collection(ResourceType, {
 		sync: function(){
+			var collection = this;
 			// Load the ResourceTypes
 			http().get('/rbs/types').done(function(rts){
 				var resourceTypes = Array();
 				_.each(rts, function(rt){
 					resourceTypes.push(new ResourceType(rt));
 				});
-				this.load(resourceTypes);
 				// Load the Resources
 				http().get('/rbs/resources').done(function(resources){
 					// Load each ResourceType's collection with associated Resources
 					var groupedResources  = _.groupBy(resources, function(resource) {
 						return resource.type_id;
 					});
+					var actions = resourceTypes.length;
 					_.each(resourceTypes, function(resourceType){
 						if (_.has(groupedResources, resourceType.id)) {
-							resourceType.resources.addRange(groupedResources[resourceType.id]);
+							resourceType.resources.addRange(groupedResources[resourceType.id], null, false);
 						}
+						collection.push(resourceType);
 					});
 				});
 			}.bind(this));
