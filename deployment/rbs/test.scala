dba9537b8e193af156ac3f82f087ebfd17baf9df
dboissin <damien.boissin@web-education.net>
2014-12-14 16:59:50 +0100

    [Evo] add deployment infos

---
+++ deployment/rbs/test.scala
@@ -0,0 +1,6 @@
+  .group("Rbs Scenario") {
+    net.atos.entng.rbs.test.integration.RbsScenario.scnCreateTeachers
+  }
+  .group("Rbs Scenario") {
+    net.atos.entng.rbs.test.integration.RbsScenario.scnCreateBookings
+  }
