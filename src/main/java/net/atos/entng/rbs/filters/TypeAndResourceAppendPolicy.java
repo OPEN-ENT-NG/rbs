db56da4600f82cb9bee92e0a077978b9e822d36d
a629817 <Contacts1997>
2016-05-13 18:26:29 +0200

    Licencing

--- src/main/java/net/atos/entng/rbs/filters/TypeAndResourceAppendPolicy.java
+++ src/main/java/net/atos/entng/rbs/filters/TypeAndResourceAppendPolicy.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright © Région Nord Pas de Calais-Picardie,  Département 91, Région Aquitaine-Limousin-Poitou-Charentes, 2016.
+ *
+ * This file is part of OPEN ENT NG. OPEN ENT NG is a versatile ENT Project based on the JVM and ENT Core Project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * For the sake of explanation, any module that communicate over native
+ * Web protocols, such as HTTP, with OPEN ENT NG is outside the scope of this
+ * license and could be license under its own terms. This is merely considered
+ * normal use of OPEN ENT NG, and does not fall under the heading of "covered work".
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 package net.atos.entng.rbs.filters;
 
 import static net.atos.entng.rbs.BookingStatus.SUSPENDED;
